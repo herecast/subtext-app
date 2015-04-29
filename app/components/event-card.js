2d9783c75a249b6cca9e00d9d22eb2ff76a1dd1c
Patrick Berkeley <patrickberkeley@gmail.com>
2015-04-29 19:21:50 -0400

    Fleshes out event show header details, and starts the layout.

--- app/components/event-card.js
+++ app/components/event-card.js
@@ -2,12 +2,5 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Component.extend({
-  classNames: ['Card'],
-
-  timeRange: function() {
-    const startTime = moment(this.get('event.startsAt')).format('LT');
-    const endTime = moment(this.get('event.endsAt')).format('LT');
-
-    return `${startTime} - ${endTime}`;
-  }.property('event.{startsAt,endsAt}')
+  classNames: ['Card']
 });
