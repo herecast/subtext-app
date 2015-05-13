8368734291d7ca107821aad8fb89734c4539715e
Brendan Fay <fay.brendan@gmail.com>
2015-05-13 16:17:22 -0400

    Adds truncation component.

--- app/components/event-card.js
+++ app/components/event-card.js
@@ -1,31 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['Card'],
-
-  truncatedTitle: function() {
-    const title = this.get('event.title');
-
-    if (title) {
-      if (title.length > 35) {
-        const truncatedtitle = title.substring(0, 35);
-        return `${truncatedtitle}...`;
-      } else {
-        return title;
-      }
-    }
-  }.property('event.title'),
-
-  truncatedContent: function() {
-    const content = this.get('event.content');
-
-    if (content) {
-      if (content.length > 90) {
-        const truncatedContent = content.substring(0, 90);
-        return `${truncatedContent}...`;
-      } else {
-        return content;
-      }
-    }
-  }.property('event.content')
+  classNames: ['Card']
 });
---
+++ app/components/x-truncate.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  truncatedText: function() {
+    const text = this.get('text'),
+          maxLength = this.get('maxLength');
+
+    if (text) {
+      if (text.length > maxLength) {
+        const truncatedText = text.substring(0, maxLength);
+        return `${truncatedText}...`;
+      } else {
+        return text;
+      }
+    }
+  }.property('text')
+
+});
