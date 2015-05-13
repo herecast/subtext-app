71a82255e0a0abd944c6b9f75f1e08d0893d67d2
Brendan Fay <fay.brendan@gmail.com>
2015-05-13 13:57:16 -0400

    Updates event card styles to match updated design.

--- app/components/event-card.js
+++ app/components/event-card.js
@@ -3,12 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['Card'],
 
+  truncatedTitle: function() {
+    const title = this.get('event.title');
+
+    if (title) {
+      if (title.length > 35) {
+        const truncatedtitle = title.substring(0, 35);
+        return `${truncatedtitle}...`;
+      } else {
+        return title;
+      }
+    }
+  }.property('event.title'),
+
   truncatedContent: function() {
     const content = this.get('event.content');
 
     if (content) {
-      if (content.length > 100) {
-        const truncatedContent = content.substring(0, 100);
+      if (content.length > 90) {
+        const truncatedContent = content.substring(0, 90);
         return `${truncatedContent}...`;
       } else {
         return content;
