3f3bb5df78588f7bd0a94e91d303ee62f858bf9a
Brendan Fay <fay.brendan@gmail.com>
2015-05-14 17:56:27 -0400

    Adds helper requirements to filter component tests.

--- tests/unit/components/event-location-filter-test.js
+++ tests/unit/components/event-location-filter-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleForComponent('event-location-filter', {
   // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['helper:fa-icon']
 });
 
 test('it renders', function(assert) {
