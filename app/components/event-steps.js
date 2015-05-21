8ea696b45267314ab551220e5173b659fb1710ce
Brendan Fay <fay.brendan@gmail.com>
2015-05-21 17:37:12 -0400

    Updates event steps to display properly on gray background.

--- app/components/event-steps.js
+++ app/components/event-steps.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['EventSteps'],
   step1Complete: Ember.computed.equal('step1State', 'complete'),
   step2Complete: Ember.computed.equal('step2State', 'complete'),
   step3Complete: Ember.computed.equal('step3State', 'complete'),
