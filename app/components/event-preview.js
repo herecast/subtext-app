c3689c5dd731068f21887b7d8136a4427d03dc76
Brendan Fay <fay.brendan@gmail.com>
2015-05-22 14:14:21 -0400

    Re-arranges components/partials to allow full width event preview.

--- app/components/event-preview.js
+++ app/components/event-preview.js
@@ -1,19 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    publish() {
-      const event = this.get('event');
-      event.publish();
-      this.sendAction('afterPublish');
-    },
-
-    discard() {
-      if (confirm('Are you sure you want to discard this event?')) {
-        const event = this.get('event');
-        event.destroyRecord();
-        this.sendAction('afterDiscard');
-      }
-    }
-  }
 });
