220a2515b6cde37d36e5d577b6921be1e4d5a514
beerlington <pete@lette.us>
2015-05-22 11:24:32 -0400

    Updates all placeholder text on event details form

--- app/components/event-form-cost.js
+++ app/components/event-form-cost.js
@@ -14,7 +14,7 @@ export default Ember.Component.extend({
     } else if (this.get('isDonation')) {
       return 'Add details if you’d like';
     } else if (this.get('isNotApplicable')) {
-      return 'For municipal meetings or other public events where cost is not relevant';
+      return 'For public events where cost is not relevant';
     }
   }.property('costType'),
 
