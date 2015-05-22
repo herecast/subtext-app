1c54847b347d9eba9e523c7db018ea018bf4a30f
beerlington <pete@lette.us>
2015-05-22 15:37:07 -0400

    Fixes current user error handling
    The current_user endpoint returns a 404 if the user is not logged in. We
    can just rescue this and proceed with them logged out.
    
    This also addresses an issue where the rejected state made the session
    think it had a currentUser object, but it was really just a promise.

--- app/services/session.js
+++ app/services/session.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  currentUser: function() {
+  getCurrentUser: function() {
     // The current user endpoint does not take an ID, so we pass 'self' so that
     // it requests a single resource
-    return this.store.find('current-user', 'self');
-  }.property()
+    this.store.find('current-user', 'self').then((user) => {
+      this.set('currentUser', user);
+    }).catch(() => {
+      this.set('currentUser', null);
+    });
+  }
 });
