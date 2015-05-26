36a6026bff2b81a8b096188a19d8de3c514b9c2f
Brendan Fay <fay.brendan@gmail.com>
2015-05-26 13:48:47 -0400

    Adds pubdate and styles to related content.

--- app/models/similar-content.js
+++ app/models/similar-content.js
@@ -1,9 +1,15 @@
 import DS from 'ember-data';
+import moment from 'moment';
 
 export default DS.Model.extend({
   author: DS.attr('string'),
   contentUrl: DS.attr('string'),
   eventInstanceId: DS.attr('number'),
   snippet: DS.attr('string'),
-  title: DS.attr('string')
+  title: DS.attr('string'),
+  pubdate: DS.attr('date'),
+
+  formattedPubdate: function() {
+     return moment(this.get('pubdate')).format('MM/DD/YYYY');
+  }.property('pubdate')
 });
