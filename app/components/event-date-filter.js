1b7c56859b6821aa1243f27208fc6166e2f047e4
beerlington <pete@lette.us>
2015-05-27 18:18:11 -0400

    Fixes some UX issues with the filters
    * getCurrentUser returns promise to ensure the current user is available
      before any other content.
    * Fixes date filter so it always shows the correct display text
      depending on which dates have been chosen.
    * Use the current user's location for the location filter
    
    One piece of slightly confusing logic is that we are now defaulting to
    Today for logged out users and This Week for logged in users. I believe
    the reason for this is because logged out users get results for the
    entire Upper Valley, but logged in users are associated with a more
    narrow region.

--- app/components/event-date-filter.js
+++ app/components/event-date-filter.js
@@ -1,42 +1,84 @@
 import Ember from 'ember';
 import moment from 'moment';
 
-const dateFormart = 'YYYY-MM-DD';
+const dateFormat = 'YYYY-MM-DD';
+
+function today() {
+  return moment().format(dateFormat);
+}
+
+function startOfWeek() {
+  return moment().startOf('week').format(dateFormat);
+}
+
+function endOfWeek() {
+  return moment().endOf('week').format(dateFormat);
+}
+
+function startOfWeekend() {
+  return moment().endOf('week').format(dateFormat);
+}
+
+function endOfWeekend() {
+  return moment().endOf('week').add(1, 'day').format(dateFormat);
+}
+
+function startOfMonth() {
+  return moment().startOf('month').format(dateFormat);
+}
+
+function endOfMonth() {
+  return moment().endOf('month').format(dateFormat);
+}
 
 export default Ember.Component.extend({
   classNames: ['dropdown'],
 
-  dateSummary: 'This Week',
   showCustomCalendar: false,
+
+  dateSummary: function() {
+    const start = this.get('startDate');
+    const stop = this.get('stopDate');
+
+    if (start === today() && stop === today()) {
+      return 'Today';
+    } else if (start === startOfWeek() && stop === endOfWeek()) {
+      return 'This Week';
+    } else if (start === startOfWeekend() && stop === endOfWeekend()) {
+      return 'This Weekend';
+    } else if (start === startOfMonth() && stop === endOfMonth()) {
+      return 'This Month';
+    } else if (Ember.isPresent(start) && Ember.isPresent(stop)) {
+      if (start === stop) {
+        return moment(start).format('MMM D');
+      } else {
+        return `${moment(start).format('MMM D')} - ${moment(stop).format('MMM D')}`;
+      }
+    }
+  }.property('startDate', 'stopDate'),
 
   actions: {
     setDate(period) {
       let startDate, stopDate;
 
       if (period === 'today') {
-        startDate = moment();
-        stopDate = moment();
-
-        this.set('dateSummary', 'Today');
+        startDate = today();
+        stopDate = today();
       } else if (period === 'week') {
-        startDate = moment().startOf('week');
-        stopDate = moment().endOf('week');
-
-        this.set('dateSummary', 'This Week');
+        startDate = startOfWeek();
+        stopDate = endOfWeek();
       } else if (period === 'weekend') {
-        startDate = moment().endOf('week');
-        stopDate = moment().endOf('week').add(1, 'day');
-
-        this.set('dateSummary', 'This Weekend');
+        startDate = startOfWeekend();
+        stopDate = endOfWeekend();
       } else if (period === 'month') {
-        startDate = moment().startOf('month');
-        stopDate = moment().endOf('month');
-
-        this.set('dateSummary', 'This Month');
+        startDate = startOfMonth();
+        stopDate = endOfMonth();
       }
 
-      this.set('startDate', startDate.format(dateFormart));
-      this.set('stopDate', stopDate.format(dateFormart));
+      this.setProperties({
+        startDate: startDate,
+        stopDate: stopDate
+      });
     },
 
     chooseDates() {
@@ -46,12 +88,10 @@ export default Ember.Component.extend({
     afterCustomSelect() {
       const startDate = moment(this.get('customStartDate'));
       const stopDate = moment(this.get('customEndDate'));
-      const dateSummary = `${startDate.format('L')} - ${stopDate.format('L')}`;
 
       this.setProperties({
-        startDate: startDate.format(dateFormart),
-        stopDate: stopDate.format(dateFormart),
-        dateSummary: dateSummary,
+        startDate: startDate.format(dateFormat),
+        stopDate: stopDate.format(dateFormat),
         showCustomCalendar: false
       });
     }
