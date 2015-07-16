0fca4e53c4ecc57ea896765e7dbc823a8df1a583
beerlington <pete@lette.us>
2015-07-16 17:12:43 -0400

    Fixes missing hours on event show page
    This fixes an issue where other event instances were missing their
    hours. It's a copy/paste from the event instance model, but they don't
    have anything else in common, so I want to keep them as separate
    properties for now.

--- app/models/other-event-instance.js
+++ app/models/other-event-instance.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import moment from 'moment';
 
@@ -8,5 +9,16 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   formattedDate: function() {
     return this.get('startsAt').format('MMMM D, YYYY');
-  }.property('startsAt')
+  }.property('startsAt'),
+
+  formattedHours: function() {
+    const startTime = this.get('startsAt').format('LT');
+
+    if (Ember.isEmpty(this.get('endsAt'))) {
+      return `${startTime}`;
+    } else {
+      const endTime = this.get('endsAt').format('LT');
+      return `${startTime} - ${endTime}`;
+    }
+  }.property('startsAt', 'endsAt')
 });
