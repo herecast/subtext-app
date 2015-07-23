8ee25df67828dca1e3ccf51b7b41789ec3d7b8c7
beerlington <pete@lette.us>
2015-07-23 16:54:29 -0400

    Revert "Merge pull request #223 from AgilionApps/category-permalink"
    This reverts commit ca6b657527179ff5c0d03fb0acc338e7337ff5c6, reversing
    changes made to 705b445b599c249ccbbace41e5fb3efe169e87ab.
    
    This is being reverted because the change broke refreshing or landing
    directly on an event show page from outside of the application. The next
    commit will fix this issue.

--- app/routes/events/category.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model(params) {
-    let category = params.category.capitalize().replace('-', ' ');
-
-    this.transitionTo('events.index', {queryParams: {category: category}});
-  }
-});
