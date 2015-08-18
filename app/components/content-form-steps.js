3976a3281faa0070cbf53c758276167a4208e2fb
Brendan Fay <fay.brendan@gmail.com>
2015-08-18 16:53:34 -0400

    Employs bootstrap grid system to make content forms responsive.

--- app/components/content-form-steps.js
+++ app/components/content-form-steps.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['ContentFormSteps'],
+  classNames: ['row'],
   step1Complete: Ember.computed.equal('step1State', 'complete'),
   step2Complete: Ember.computed.equal('step2State', 'complete'),
   step3Complete: Ember.computed.equal('step3State', 'complete'),
