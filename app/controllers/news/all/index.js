3f1dea8b95949bd3440a0a471f41f23d489d30dd
Brendan Fay <fay.brendan@gmail.com>
2015-08-25 17:06:38 -0400

    Adds gray background to news index on mobile as per design.

--- app/controllers/news/all/index.js
+++ app/controllers/news/all/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   refreshParam: Ember.inject.service('refresh-param'),
+  secondaryBackgroundMobile: true,
 
   queryParams: ['query', 'location', 'publication', 'r'],
 
