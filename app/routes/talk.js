1c8e813864f086f59a77136b1dad4d070539f3d1
beerlington <pete@lette.us>
2015-08-27 17:11:40 -0400

    Require login to see dashboard or talk section
    Users cannot see the talk section of the app because that content is
    restricted to people who are part of a specific community.

--- app/routes/talk.js
+++ app/routes/talk.js
@@ -2,7 +2,4 @@ import Ember from 'ember';
 import Authorized from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(Authorized, {
-  model: function() {
-    return this.get('session.currentUser');
-  }
 });
