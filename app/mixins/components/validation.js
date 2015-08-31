117f4dcb20ca4fab5c0c1afde15fb08320054cfb
beerlington <pete@lette.us>
2015-08-31 15:25:01 -0400

    Validates phone and email on market listing page

--- app/mixins/components/validation.js
+++ app/mixins/components/validation.js
@@ -13,5 +13,20 @@ export default Ember.Mixin.create({
     } else {
       this.set(`errors.${attrName}`, 'Cannot be blank');
     }
+  },
+
+  hasValidEmail(email) {
+    // From https://html.spec.whatwg.org/multipage/forms.html#valid-e-mail-address
+    // Retrieved 2014-01-14
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    return Ember.isBlank(email) || emailRegex.test(email);
+  },
+
+  hasValidPhone(phone) {
+    const requiredDigits = 10;
+    const actualDigits = phone.replace(/[^0-9]/g,"").length;
+
+    return Ember.isBlank(phone) || requiredDigits === actualDigits;
   }
 });
