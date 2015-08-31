ade64b2469182cbf6fdc8d00b6f04da844cc89df
beerlington <pete@lette.us>
2015-08-31 13:02:54 -0400

    Fixes background color when editing content
    We need to use the secondaryBackground flag to tell Ember to use the
    darker background on these pages.

--- tests/unit/controllers/events/edit/details-test.js
+++ tests/unit/controllers/events/edit/details-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:events/edit/promotion', {
+moduleFor('controller:events/edit/details', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
