5249c76652a116117664630418b273e9b92d2b1b
beerlington <pete@lette.us>
2015-09-03 13:41:52 -0400

    Fixes an issue with missing index content
    If an API request for /contents doesn't return all 14, we were
    still rendering the cards and showing incomplete data. This fixes it
    by simply hidding the content if it's not available.

--- app/components/content-card.js
+++ app/components/content-card.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNameBindings: ['missingContent:hidden'],
+  missingContent: Ember.computed.empty('item'),
+
   showLink: true,
   isNews: Ember.computed.equal('item.contentType', 'news'),
   isTalk: Ember.computed.equal('item.contentType', 'talk'),
