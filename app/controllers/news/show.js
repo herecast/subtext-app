b4fcbee36fe331500254ba9ec83e9e26161b1823
beerlington <pete@lette.us>
2015-09-04 09:08:56 -0400

    Fixes news caption/credit logic
    I had the right method name, but apparently the logic was too hard for
    me when I coded it the first time.

--- app/controllers/news/show.js
+++ app/controllers/news/show.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   refreshParam: Ember.inject.service('refresh-param'),
 
   hasCaptionOrCredit: function() {
-    return Ember.isPresent(this.get('model.bannerImage.caption')) &&
+    return Ember.isPresent(this.get('model.bannerImage.caption')) ||
       Ember.isPresent(this.get('model.bannerImage.credit'));
   }.property('model.bannerImage.{caption,credit}')
 });
