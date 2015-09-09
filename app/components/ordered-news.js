4adad252b788caafd97f481262101013c3bb0900
Brendan Fay <fay.brendan@gmail.com>
2015-09-08 20:00:59 -0400

    Removes expandability from News index in favor of pagination.

--- app/components/ordered-news.js
+++ app/components/ordered-news.js
@@ -1,20 +1,9 @@
 import Ember from 'ember';
-import ExpandableContent from '../mixins/components/expandable-content';
 
-export default Ember.Component.extend(ExpandableContent, {
+export default Ember.Component.extend({
   news: [],
 
-  content: function () {
-    return this.get('news').sortBy('publishedAt');
-  }.property('news.@each.publishedAt'),
-
-  firstItem: function() {
-    return this.get('contentToDisplay')[0];
-  }.property('contentToDisplay.[]'),
-
-  restOfContent: function() {
-    return this.get('contentToDisplay').slice(1);
-  }.property('contentToDisplay.[]')
-
-
+  orderedNews: function () {
+    return this.get('news').sortBy('publishedAt').toArray().reverse();
+  }.property('news.@each.publishedAt')
 });
