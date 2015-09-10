c0f185c69718fccbf6f5bd37ed6ae35dfe8dcd18
beerlington <pete@lette.us>
2015-09-10 16:34:07 -0400

    Updates consumer app uri
    This is how the API knows which database to serve data from.

--- app/lib/devise-authorizer.js
+++ app/lib/devise-authorizer.js
@@ -2,7 +2,7 @@ import DeviseAuth from 'simple-auth-devise/authorizers/devise';
 
 export default DeviseAuth.extend({
   authorize(jqXHR, requestOptions) {
-    jqXHR.setRequestHeader('Consumer-App-Uri', 'http://qa-demo.subtext.org');
+    jqXHR.setRequestHeader('Consumer-App-Uri', 'http://stage-consumer.subtext.org');
 
     this._super(jqXHR, requestOptions);
   }
