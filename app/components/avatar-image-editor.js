da8d0e4b50b29c1ff5764325d7ae785d6ded309b
Brendan Fay <fay.brendan@gmail.com>
2015-09-10 11:26:33 -0400

    Adds avatar image upload support to dashboard.

---
+++ app/components/avatar-image-editor.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import config from './../config/environment';
+import ajax from 'ic-ajax';
+
+export default Ember.Component.extend({
+  isEditingImage: false,
+
+  actions: {
+    changePhoto() {
+      this.toggleProperty('isEditingImage');
+      this.$('.ContentForm-fileField').click();
+    },
+
+    savePhoto() {
+      this.toggleProperty('isEditingImage');
+      const url = `/${config.API_NAMESPACE}/current_user`;
+      const data = new FormData();
+
+      if (this.get('currentUser.image')) {
+        data.append('current_user[image]', this.get('currentUser.image'));
+
+        return ajax(url, {
+          data: data,
+          type: 'PUT',
+          contentType: false,
+          processData: false
+        });
+      }
+    }
+  }
+});
