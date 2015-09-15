349d6084c6be55a880dc37c8cbc511ff9cdd78e7
Brendan Fay <fay.brendan@gmail.com>
2015-09-15 15:34:27 -0400

    Sorts talk cards within group by descending publishAt.

--- app/components/grouped-talks.js
+++ app/components/grouped-talks.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   talks: [],
 
   groupedTalks: function() {
-    const talks = this.get('talks').sortBy('publishedAt');
+    const talks = this.get('talks').sortBy('publishedAt').toArray().reverse();
     const groupBy = 'publishedAt';
 
     return buildGroup(talks, groupBy, 'dddd, MMMM D', function(date) {
