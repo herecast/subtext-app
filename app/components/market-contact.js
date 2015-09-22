740b539301581b8d59921dc00ad5b068ee0f6e79
beerlington <pete@lette.us>
2015-09-22 14:25:40 -0400

    Addresses market post UX issues
    1. Adds mailto link for all email addresses, not just listservs
    2. The reply button UX is consistent between listserv and non-listserv
       postings

--- app/components/market-contact.js
+++ app/components/market-contact.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import moment from 'moment';
 
 const isPresent = Ember.isPresent;
 
@@ -24,34 +23,34 @@ export default Ember.Component.extend({
   isFromListServ: Ember.computed.not('post.hasContactInfo'),
 
   mailToHref: function() {
-    if (this.get('isFromListServ')) {
-      const mailTo = `mailto:${this.get('post.authorEmail')}`;
-      const formattedPublishDay = moment(this.get('post.publishedAt')).format('MMMM Do YYYY');
-      const formattedPublishTime = moment(this.get('post.publishedAt')).format('HH:mm a');
-      const formattedPublishDate = `${formattedPublishDay} at ${formattedPublishTime}`;
-      const firstLine = `On ${formattedPublishDate}, ${this.get('post.authorName')} wrote:`;
-      let tmp = document.createElement("DIV");
-      tmp.innerHTML = this.get('post.content');
-      const sanitizedContent = tmp.textContent || tmp.innerText || "";
-      const body = `%0D%0A%0D%0A${firstLine}%0D%0A%0D%0A${sanitizedContent}`;
-      return `${mailTo}?subject=${this.get('post.title')}&body=${body}`;
-    }
-  }.property('post.authorName', 'post.authorEmail', 'post.title', 'post.content', 'isFromListServ'),
+    const mailTo = `mailto:${this.get('email')}`;
+    const formattedPublishDate = this.get('post.publishedAt').format('MMMM Do YYYY [at] HH:mm a');
+    const firstLine = `On ${formattedPublishDate}, ${this.get('post.authorName')} wrote:`;
+    let tmp = document.createElement("DIV");
+    tmp.innerHTML = this.get('post.content');
+    const sanitizedContent = tmp.textContent || tmp.innerText || "";
+    const body = `%0D%0A%0D%0A${firstLine}%0D%0A%0D%0A${sanitizedContent}`;
+    return `${mailTo}?subject=${this.get('post.title')}&body=${body}`;
+  }.property('post.authorName', 'email', 'post.title', 'post.content'),
 
   actions: {
     toggleInfo() {
       if (!this.get('showInfo')) {
         this.toggleProperty('showInfo');
 
-        this.set('isLoading', true);
+        if (this.get('isFromListServ')) {
+          this.set('email', this.get('post.authorEmail'));
+        } else {
+          this.set('isLoading', true);
 
-        this.get('post').loadContactInfo().then(() => {
-          this.setProperties({
-            isLoading: false,
-            email: this.get('post.contactEmail'),
-            phone: this.get('post.contactPhone'),
+          this.get('post').loadContactInfo().then(() => {
+            this.setProperties({
+              isLoading: false,
+              email: this.get('post.contactEmail'),
+              phone: this.get('post.contactPhone'),
+            });
           });
-        });
+        }
       }
     }
   }
