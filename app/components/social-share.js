aa5613ae966f48896352ee966fb9f01d45c87b6a
beerlington <pete@lette.us>
2015-09-25 13:36:28 -0400

    Adds email sharing functionality
    This will open an email composition window with the subject and body
    prepopulated.

--- app/components/social-share.js
+++ app/components/social-share.js
@@ -4,6 +4,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['SocialShare'],
 
+  mailtoLink: function() {
+    const href = `${location.protocol}//${location.host}${location.pathname}`;
+    const title = this.get('title');
+    const sharedBy = this.get('sharedBy');
+    const subject = `Shared with you: ${title}`;
+    const body = `${sharedBy} shared the following content from dailyUV.com with you: ${href}`;
+
+    return `mailto:?subject=${subject}&body=${body}`;
+  }.property('title', 'sharedBy'),
+
   actions: {
     shareFacebook() {
       FB.ui({
