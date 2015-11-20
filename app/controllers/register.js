cfe495ba07c7fd0bb3bed51b6b8cb7f6fb49b2e5
beerlington <pete@lette.us>
2015-11-20 12:42:30 -0500

    Improves registration UX
    When registering, no errors were displayed until the form was completely
    filled out. This made it confusing because users might input invalid
    data, but not be able to see the errors. This adds additional logic to
    make sure they see the errors as soon as they submit the form.

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -3,49 +3,121 @@ import ajax from 'ic-ajax';
 import config from '../config/environment';
 import Validation from '../mixins/components/validation';
 
-const isPresent = Ember.isPresent;
+const {
+  computed,
+  get,
+  isBlank,
+  isPresent,
+  observer,
+  RSVP,
+  set
+} = Ember;
 
 export default Ember.Controller.extend(Validation, {
   secondaryBackground: true,
 
-  isValid: function() {
-    const email = this.get('email');
-    const password = this.get('password');
-    const locationId = this.get('locationId');
-    const name = this.get('name');
-    const termsAccepted = this.get('termsAccepted');
+  init() {
+    this._super(...arguments);
+    this.resetProperties();
+  },
 
-    return isPresent(email) && this.hasValidEmail(email) &&
-      isPresent(password) && isPresent(locationId) && isPresent(name) &&
-      termsAccepted;
-  }.property('email', 'password', 'locationId', 'name', 'termsAccepted'),
+  resetProperties() {
+    set(this, 'showErrors', false);
+    set(this, 'requestErrors', {});
+  },
+
+  isValid: computed('allErrors.{email,password,locationId,name,termsAccepted}', function() {
+    const errors = get(this, 'allErrors');
+
+    const emailError = get(errors, 'email');
+    const passwordError = get(errors, 'password');
+    const locationIdError = get(errors, 'locationId');
+    const nameError = get(errors, 'name');
+    const termsAcceptedError = get(errors, 'termsAccepted');
+
+    return !emailError && !passwordError && !locationIdError && !nameError &&
+      !termsAcceptedError;
+  }),
+
+  error: computed('showErrors','allErrors.{email,password,locationId,name,termsAccepted}', function() {
+    if (get(this, 'showErrors')) {
+      return get(this, 'allErrors');
+    } else {
+      return Ember.Object.create();
+    }
+  }),
+
+  // If the API returns an error, we need to reset it if the user changes
+  // the value in the form so it's no longer displayed.
+  updateRequestErrors: observer('email', function() {
+    const email = get(this, 'email');
+
+    if (isPresent(email)) {
+      set(this, 'requestErrors.email', null);
+    }
+  }),
+
+  allErrors: computed(
+    'requestErrors', 'email', 'password', 'locationId',
+    'name', 'termsAccepted', function() {
+
+    const email = get(this, 'email');
+    const password = get(this, 'password');
+    const locationId = get(this, 'locationId');
+    const name = get(this, 'name');
+    const termsAccepted = get(this, 'termsAccepted');
+
+    let emailErrorMsg;
+    const emailError = get(this, 'requestErrors.email');
+
+    if (isBlank(email)) {
+      emailErrorMsg = 'Email cannot be blank';
+    } else if (!this.hasValidEmail(email)) {
+      emailErrorMsg = 'Email format is invalid';
+    } else if (emailError) {
+      emailErrorMsg = emailError;
+    }
+
+    return Ember.Object.create({
+      email: emailErrorMsg,
+      password: isPresent(password) ? null : 'Password cannot be blank',
+      locationId: isPresent(locationId) ? null : 'Location must be selected',
+      name: isPresent(name) ? null : 'Name cannot be blank',
+      termsAccepted: termsAccepted ? null : 'You must agree to the terms of service'
+    });
+  }),
 
   actions: {
     register(callback) {
+      set(this, 'showErrors', true);
+
       const url = `${config.API_NAMESPACE}/users/sign_up`;
       const password = this.get('password');
       const email = this.get('email');
 
-      ajax(url, {
-        type: 'POST',
-        dataType: "json",
-        data: {
-          user: {
-            name: this.get('name'),
-            location_id: this.get('locationId'),
-            email: email,
-            password: password,
-            password_confirmation: password
+      if (get(this, 'isValid')) {
+        ajax(url, {
+          type: 'POST',
+          dataType: "json",
+          data: {
+            user: {
+              name: this.get('name'),
+              location_id: this.get('locationId'),
+              email: email,
+              password: password,
+              password_confirmation: password
+            }
           }
-        }
-      }).then(() => {
-        this.transitionTo('register.complete');
-      }).catch((response) => {
-        this.set('error', response.jqXHR.responseJSON.errors);
-        const promise = new Ember.RSVP.Promise((resolve, reject) => { reject(); });
+        }).then(() => {
+          this.transitionTo('register.complete');
+        }).catch((response) => {
+          set(this, 'requestErrors', response.jqXHR.responseJSON.errors);
 
-        callback(promise);
-      });
+          callback(RSVP.reject());
+        });
+      } else {
+        callback(RSVP.resolve());
+      }
     }
   }
 });
