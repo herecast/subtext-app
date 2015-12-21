0fea458866e206f04eebdb3647a1105c3afc1975
beerlington <pete@lette.us>
2015-12-21 15:28:21 -0500

    Fixes incorrect weather when user changes location
    This addresses an issue where the weather wasn't being updated when a
    user changed their location.

--- app/pods/components/x-weather/component.js
+++ app/pods/components/x-weather/component.js
@@ -24,5 +24,15 @@ export default Ember.Component.extend({
     ajax(url).then((response) => {
       set(this, 'weatherHTML', response);
     });
+  },
+
+  didUpdateAttrs(attrs) {
+    this._super(...arguments);
+
+    // When the user's location changes, reload the weather so that we are
+    // always showing the weather for their current location.
+    if (attrs.oldAttrs.location !== attrs.newAttrs.location) {
+      this._getWeather();
+    }
   }
 });
