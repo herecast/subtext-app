8a3ab1393e19b47e707c1b9871214841467cc6ca
beerlington <pete@lette.us>
2015-12-22 08:29:59 -0500

    Removes unneeded "autoload" code from weather component
    The autoload=true was just a hack so that the unit tests didn't try and
    make an AJAX request to the API during the test. This updates the test
    to rely on the mirage server to handle the API request, allowing the
    template to be rendered like it is in the browser.

--- app/pods/components/x-weather/component.js
+++ app/pods/components/x-weather/component.js
@@ -2,20 +2,15 @@ import Ember from 'ember';
 import ajax from 'ic-ajax';
 import config from 'subtext-ui/config/environment';
 
-const { get, set } = Ember;
+const { set } = Ember;
 
 export default Ember.Component.extend({
   classNames: ['Weather'],
-  autoload: false,
 
   init() {
     this._super(...arguments);
 
-    const autoload = get(this, 'autoload');
-
-    if (autoload) {
-      this._getWeather();
-    }
+    this._getWeather();
   },
 
   _getWeather() {
--- tests/integration/pods/components/x-weather/component-test.js
+++ tests/integration/pods/components/x-weather/component-test.js
@@ -1,15 +1,24 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import wait from 'ember-test-helpers/wait';
 import hbs from 'htmlbars-inline-precompile';
+import startMirage from '../../../../helpers/setup-mirage';
 
 moduleForComponent('x-weather', 'Integration | Component | x weather', {
-  integration: true
+  integration: true,
+  setup: function() {
+    startMirage(this.container);
+  }
 });
 
 test('it renders', function(assert) {
+  assert.expect(1);
+
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
   this.render(hbs`{{x-weather}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  return wait().then(() => {
+    assert.equal(this.$().text().trim(), '80° Clear');
+  });
 });
