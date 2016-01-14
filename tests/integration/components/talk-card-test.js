2eb17428740cceb4c88266f3489927b9121558b0
Nik Petersen <npetersen@viciware.com>
2016-01-14 08:03:33 -0800

    updated state class test for talk cards to reduce unneeded code

--- tests/integration/components/talk-card-test.js
+++ tests/integration/components/talk-card-test.js
@@ -9,20 +9,20 @@ moduleForComponent('talk-card', 'Integration | Component | Talk Card', {
 
 test('When Talk comments has comments, it has a stacked state class. Otherwise it does not', function(assert){
   assert.expect(2);
-    
-  this.set('talk',Ember.Object.create({
+  let talk = Ember.Object.create({
     commentCount: 1
-  }));
+  });
+  
+  this.set('talk',talk);
+  
   this.render(hbs`{{component 'talk-card' class='TalkCard' talk=talk}}`);
-  var $TalkCard = this.$('.TalkCard');
+  
+  let $TalkCard = this.$('.TalkCard');
+  
   assert.equal($TalkCard.hasClass('TalkCard--stacked'), true, 'Has stacked modifier class');
   
   // Part two
-  this.set('talk',Ember.Object.create({
-    commentCount: 0
-  }));
-  this.render(hbs`{{component 'talk-card' class='TalkCard' talk=talk}}`);
-  $TalkCard = this.$('.TalkCard');
+  this.set('talk.commentCount',0);
   assert.equal($TalkCard.hasClass('TalkCard--stacked'), false, 'Does not have stacked modifier class');
   
 });
