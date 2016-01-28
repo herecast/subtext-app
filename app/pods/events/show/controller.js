26ce2a7b72696b156fe2f6fff202b15b3ca327a8
beerlington <pete@lette.us>
2016-01-28 13:13:31 -0500

    Adds tracking when user clicks on google maps link

--- app/pods/events/show/controller.js
+++ app/pods/events/show/controller.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
+import TrackEvent from 'subtext-ui/mixins/track-event';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(TrackEvent, {
   queryParams: ['scrollTo'],
-  scrollTo: null
+  scrollTo: null,
+
+  actions: {
+    trackMapClick() {
+      this.trackEvent('selectNavControl', {
+        navControlGroup: 'Event Info',
+        navControl: 'Event Map'
+      });
+    }
+  }
 });
