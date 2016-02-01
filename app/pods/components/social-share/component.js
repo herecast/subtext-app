2370e01a67a4fa701ae92389178b2d167bb0f46b
Christopher Milne <christopher.milne@gmail.com>
2016-02-01 16:56:10 -0500

    fixes mailto link causing blank tab to open

--- app/pods/components/social-share/component.js
+++ app/pods/components/social-share/component.js
@@ -35,7 +35,7 @@ export default Ember.Component.extend(TrackEvent, {
         navControl: 'email'
       });
 
-      window.open(mailto, '_blank');
+      window.location.href = mailto;
     },
 
     shareFacebook() {
