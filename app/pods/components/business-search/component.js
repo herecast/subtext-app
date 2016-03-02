0017e8e0ca53dcc1c4cf963e66e4f4280df4ac28
Christopher Milne <christopher.milne@gmail.com>
2016-03-02 09:48:02 -0500

    increases search responsiveness

--- app/pods/components/business-search/component.js
+++ app/pods/components/business-search/component.js
@@ -3,11 +3,13 @@ import Ember from 'ember';
 const {
   set,
   get,
-  run
+  run,
+  isPresent
 } = Ember;
 
 export default Ember.Component.extend({
   input: null,
+  query: null,
 
   _setInputElement() {
     const input = this.$('input');
@@ -20,7 +22,7 @@ export default Ember.Component.extend({
       const value = get(this, 'input').val();
 
       if (event.keyCode !== 9 && event.keyCode !== 13) {
-        run.debounce(this, this.updateSearchTerms, value, 600);
+        run.debounce(this, this.updateSearchTerms, value, 200);
       }
     });
   },
@@ -35,8 +37,25 @@ export default Ember.Component.extend({
 
   didInsertElement() {
     this._setInputElement();
+    // TODO replace query with actual property
+    const searchTerms = get(this, 'query')
     const $input = get(this, 'input');
-
+    // set initial value
+    if (searchTerms) {
+      $input.val(searchTerms);
+    }
     this._bindUpdateFunction($input);
+  },
+
+  didUpdateAttrs() {
+    const $input = get(this, 'input');
+    const searchTerms = get(this, 'attrs.searchTerms.value');
+    const isCategory = get(this, 'attrs.category.value');
+    // we have to manually manage the input value
+    // since there is some business logic about
+    // what should be used for the value
+    if (isPresent(searchTerms) && isCategory !== null) {
+      $input.val(searchTerms);
+    }
   }
 });
