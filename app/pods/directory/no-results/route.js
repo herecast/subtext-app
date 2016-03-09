d94b7ec8891aa0d4a71a1d9c89975b3e5310dc69
Nik Petersen <demersus@gmail.com>
2016-03-08 16:26:30 -0800

    Merge  bug/bsd-no-results into feature/bsd-master Conflicts: 	app/pods/components/directory-search-results/template.hbs 	app/pods/directory/no-results/route.js 	app/pods/directory/search/results/controller.js 	app/pods/directory/search/results/template.hbs

--- app/pods/directory/no-results/route.js
+++ app/pods/directory/no-results/route.js
@@ -1,12 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  afterModel() {
-    const currentRoute = this.container.lookup('controller:application').currentRouteName;
-
-    // redirect on page refresh or deep link
-    if (currentRoute === 'loading') {
-      this.transitionTo('directory.landing');
-    }
-  }
 });
