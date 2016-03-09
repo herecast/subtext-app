993bbb3becd4abcf1e42afa891cead54010ecc1a
Christopher Milne <christopher.milne@gmail.com>
2016-03-08 21:21:29 -0500

    enables google map to render one location when on bsd show page or many location on bsd search results page

--- app/pods/directory/search/controller.js
+++ app/pods/directory/search/controller.js
@@ -1,12 +1,38 @@
 import Ember from 'ember';
 
-const { computed, get } = Ember;
+const { computed, get, isEmpty } = Ember;
 
 export default Ember.Controller.extend({
   queryParams: ['lat', 'lng', 'query', 'category_id'],
   category_id: null,
   query: null,
   results: computed.alias('model'),
+  locationsToRender: computed('selectedLocation', function() {
+    const selectedLocation = get(this, 'selectedLocation');
+    const locations = get(this, 'locations');
+
+    return (selectedLocation) ? selectedLocation : locations;
+  }),
+  selectedResult: null,
+  selectedLocation: computed('selectedResult', function() {
+    const location = get(this, 'selectedResult');
+
+    if (isEmpty(location)) {
+      return null;
+    }
+
+    return [{
+      coords: {
+        lat: parseFloat(location.get('coords.lat')),
+        lng: parseFloat(location.get('coords.lng'))
+      },
+      title: location.get('name'),
+      content: `<h2>${location.get('name')}</h2>
+                <div><i class="fa fa-map-marker"></i> ${location.get('fullAddress')}</div>
+                <div><i class="fa fa-phone"></i> ${location.get('phone')}</div>
+                <div><a href="${location.get('directionsLink')}" target="_blank"><i class="fa fa-automobile"></i> Directions</a></div>`
+    }];
+  }),
 
   locations: computed('results.[]', 'results.@each', function () {
     const results = get(this, 'results') || [];
