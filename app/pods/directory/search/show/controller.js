d96a547ec3801e10b40d23699abceac506a16a87
Nik Petersen <demersus@gmail.com>
2016-03-18 14:11:27 -0700

    Added Claim Business button below Leave feedback button.
    * Triggers intercom conversation, or email if intercom not available.

--- app/pods/directory/search/show/controller.js
+++ app/pods/directory/search/show/controller.js
@@ -5,5 +5,15 @@ const { inject, computed } = Ember;
 export default Ember.Controller.extend({
   geo: inject.service('geolocation'),
   myCoords: computed.oneWay('geo.userLocation.coords'),
-  fromSearch: window.location.href.indexOf('?') >= 0
+  fromSearch: window.location.href.indexOf('?') >= 0,
+  actions: {
+    contactUs() {
+      let intercomButton = Ember.$('.intercom-launcher-button');
+      if(intercomButton.length > 0){
+        intercomButton[0].click();
+      }else{
+        window.location.href = "mailto:dailyuv@subtext.org?subject=My Business on dailyUV";
+      }
+    }
+  }
 });
