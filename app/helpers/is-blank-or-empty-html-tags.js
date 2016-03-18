50f7737aeadc1278ad74a34563bf5d2fdbcfdd9f
gfmartinez <g@briel.us>
2016-03-18 09:58:13 -0400

    fix test
    fix error. wrong method.
    
    fix

--- app/helpers/is-blank-or-empty-html-tags.js
+++ app/helpers/is-blank-or-empty-html-tags.js
@@ -2,14 +2,18 @@ import Ember from 'ember';
 
 
 export function isBlankOrEmptyHtmlTags(params) {
-  var blank = Ember.empty(params[0]);
+  var blank = Ember.isEmpty( params[0] );
 
   if( !blank ) {
     //params is the string, typically wrapped in <p></p> tags so never empty
     //check to see if wrapped
     let children = Ember.$("<div>" + params[0] + "</div>").children();
     //if no wraps, is the content there?
-    if( children.length <= 0 && params[0].length > 0 ) { return false; }
+    if( children.length <= 0 && params[0].length > 0 ) {
+       return false;
+     } else {
+       blank = true;
+     }
     //if children exist, we need to make sure at least one is non empty
     var inside;
 
