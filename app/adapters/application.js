7626d770a303c373ee5fff9b0a287d8348cbb721
jmadson <johnmadson@gmail.com>
2016-04-04 15:06:40 -0400

    Allowing DataAdapterMixin from ember-simple-auth to set the Authorization header without overriding it.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,23 +1,12 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 import config from './../config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-
-const { computed } = Ember;
 
 export default DS.ActiveModelAdapter.extend(DataAdapterMixin, {
   namespace: config.API_NAMESPACE,
   coalesceFindRequests: true,
   authorizer: 'authorizer:application',
-  headers: computed('session.data.secure.token', function() {
-    let headers = {
-      "Consumer-App-Uri": config['consumer-app-uri']
-    };
-
-    if (this.get('session.isAuthenticated')) {
-      headers.Authorization = `Token token="${this.get('session.data.secure.token')}" email="${this.get('session.data.secure.email')}"`;
-    }
-
-    return headers;
-  })
+  headers: {
+    "Consumer-App-Uri": config['consumer-app-uri']
+  }
 });
--- app/authorizers/application.js
+++ app/authorizers/application.js
@@ -6,12 +6,11 @@ export default Devise.extend({
   mixpanel: Ember.inject.service('mixpanel'),
 
   authorize(data, header) {
-    header('Consumer-App-Uri', config['consumer-app-uri']);
     const mixpanel = this.get('mixpanel');
     const d_id = mixpanel.getDistinctId();
     // if mixpanel is not initialized yet, getDistinctId() returns undefined
     // rather than try to wait for mixpanel to finish initializing, I'm just
-    // allowing a potential request to go untracked. It should -- at most -- 
+    // allowing a potential request to go untracked. It should -- at most --
     // be the user's direct visit to the site, all interactions after that
     // will be tracked according to their distinct ID
     if (typeof(d_id) !== 'undefined') {
