60d7ba80048060383067db682ac2e2a0c644dcc5
Christopher Milne <christopher.milne@gmail.com>
2016-04-06 08:01:22 -0400

    removes all views and associated tests

--- tests/unit/views/events/all/index-test.js
+++
@@ -1,9 +0,0 @@
-import { moduleFor, test } from 'ember-qunit';
-
-moduleFor('view:events/all/index', 'Unit | View | events/all/index');
-
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var view = this.subject();
-  assert.ok(view);
-});
--- tests/unit/views/talk/all/index-test.js
+++
@@ -1,9 +0,0 @@
-import { moduleFor, test } from 'ember-qunit';
-
-moduleFor('view:talk/all/index', 'Unit | View | talk/all/index');
-
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var view = this.subject();
-  assert.ok(view);
-});
