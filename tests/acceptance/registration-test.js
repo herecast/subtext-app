84a395330d7e0d83a4af47ce00967e0a620656a2
jmadson <johnmadson@gmail.com>
2016-04-11 14:54:44 -0400

    Acceptance Testing: Added registration acceptance tests

---
+++ tests/acceptance/registration-test.js
@@ -0,0 +1,64 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'subtext-ui/tests/helpers/module-for-acceptance';
+import { authenticateSession } from 'subtext-ui/tests/helpers/ember-simple-auth';
+import testSelector from 'subtext-ui/tests/helpers/ember-test-selectors';
+
+moduleForAcceptance('Acceptance | registration');
+
+test('clicking join link redirects to registration page', function(assert) {
+  visit('/');
+
+  click(testSelector('link', 'join-link'));
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign_up', 'it should redirect to registration link after clicking join');
+  });
+});
+
+test('registration works', function(assert) {
+  const locations = server.createList('location', 8);
+  visit('/sign_up');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign_up', 'it should be at the correct url to register');
+  });
+
+  fillIn(testSelector('component', 'location-dropdown'), locations[0].id);
+  fillIn(testSelector('component', 'register-name-input'), 'Marshall Mathers');
+  fillIn(testSelector('component', 'register-email-input'), 'slim_shady@example.com');
+  fillIn(testSelector('component', 'register-password-input'), 'willtherealslimshadypleasestandup1');
+  click('#terms');
+  click(testSelector('component', 'register-submit-button'));
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign_up/complete', 'it should be at the confirmation page after registration');
+  });
+});
+
+test('registration requires clicking I agree', function(assert) {
+  const locations = server.createList('location', 8);
+  visit('/sign_up');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign_up', 'it should be at the correct url to register');
+  });
+
+  fillIn(testSelector('component', 'location-dropdown'), locations[0].id);
+  fillIn(testSelector('component', 'register-name-input'), 'Marshall Mathers');
+  fillIn(testSelector('component', 'register-email-input'), 'slim_shady@example.com');
+  fillIn(testSelector('component', 'register-password-input'), 'willtherealslimshadypleasestandup1');
+  click(testSelector('component', 'register-submit-button'));
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign_up', 'it should not redirect if user attempts to join without clicking I agree');
+  });
+});
+
+test('visiting registration page while already authenticated redirects to root page', function(assert) {
+  authenticateSession(this.application);
+  visit('/sign_up');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/', 'it should redirect to the correct url when the user is already authenticated');
+  });
+});
