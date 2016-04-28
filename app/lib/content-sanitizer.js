d553875d1cf0d993d3bf50081535c6da153152c3
Nik Petersen <demersus@gmail.com>
2016-04-28 07:34:06 -0700

    Use jquery to ensure ul > ul are wrapped in li tags

--- app/lib/content-sanitizer.js
+++ app/lib/content-sanitizer.js
@@ -1,4 +1,15 @@
 /* global Sanitize */
+/* global jQuery */
+function fixBadLists(content) {
+  // This filter is to fix <ul><ul><li></li></ul></ul>
+  // pastes.  This can happen from google docs.
+  // The editor doesn't understand them, 
+  // and it makes it hard to undo the formatting.
+  let $content = jQuery(content).clone();
+  $content.find('ul > ul').wrap('<li>');
+  $content.find('ol > ol').wrap('<li>');
+  return $content[0];
+}
 
 export function sanitizeContent(rawContent) {
   // TODO TDD actual Sanitizer config
@@ -19,6 +30,6 @@ export function sanitizeContent(rawContent) {
       // iframe: {} // TODO responsive video embed classes
     }
   });
-
-  return s.clean_node(rawContent);
+  
+  return fixBadLists( s.clean_node(rawContent) );
 }
