6363c708ac1237c6f5003e71a535d94f7c6dee04
Christopher Milne <christopher.milne@gmail.com>
2016-05-31 19:46:35 -0400

    attempting to fix broken deployment

--- config/deploy.js
+++ config/deploy.js
@@ -1,42 +1,46 @@
+var app = 'ux2';
+var qaHost = 'https://qa-consumer.subtext.org';
+var stagingHost = 'http://stage-consumer.subtext.org';
+var productionHost = 'https://dailyuv.com';
 var path = require('path');
 var sshkey = process.env['EMBER_SSH_KEY'] ? process.env['EMBER_SSH_KEY'] : path.join(process.env['HOME'], '.ssh/id_rsa');
 
-// AWS keys should be set in the environment via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-module.exports = {
-  "production": {
-    "assets": {
-      "bucket": "subtext-consumer",
-      "prefix": "dist",
-      "distPrefix": "dist-{{SHA}}"
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {},
+    s3: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      bucket: 'subtext-consumer',
+      region: 'us-east-1',
+      prefix: 'dist',
     },
-    "index": {
-      "app": "ux2",
-      "endpoints": ["https://dailyuv.com"],
-      "privateKey": sshkey
+    'front-end-builds': {
+      app: app,
+      privateKey: sshkey
     }
-  },
-  "staging": {
-    "assets": {
-      "bucket": "subtext-consumer",
-      "prefix": "dist",
-      "distPrefix": "dist-{{SHA}}"
-    },
-    "index": {
-      "app": "ux2",
-      "endpoints": ["http://stage-consumer.subtext.org"],
-      "privateKey": sshkey
-    }
-  },
-  "qa": {
-      "assets": {
-      "bucket": "subtext-consumer",
-      "prefix": "dist",
-      "distPrefix": "dist-{{SHA}}"
-    },
-    "index": {
-      "app": "ux2",
-      "endpoints": ["https://qa-consumer.subtext.org"],
-      "privateKey": sshkey
-    }
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    ENV['front-end-builds'].endpoint = 'http://localhost:3000';
   }
+
+  if (deployTarget === 'qa') {
+    ENV.build.environment = 'production';
+    ENV['front-end-builds'].endpoint = qaHost;
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    ENV['front-end-builds'].endpoint = stagingHost;
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    ENV['front-end-builds'].endpoint = productionHost;
+  }
+
+  return ENV;
 };
