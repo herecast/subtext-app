6cd4a84de5f8c28ee8e54591d91b563f9c9c3f2d
gfmartinez <g@briel.us>
2016-09-08 15:32:34 -0400

    change bsd badge language, give two options (#1301)
    * change bsd badge language, give two options
    
    * changes based on PO feedback
    
    fix

--- app/pods/components/business-feedback-badge/component.js
+++ app/pods/components/business-feedback-badge/component.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
+const {get} = Ember;
+
 export default Ember.Component.extend({
   tagName: 'span',
-  classNames: ['BusinessFeedbackBadge']
+  classNames: ['BusinessFeedbackBadge'],
+
+  isPlural: Ember.computed('result.feedback_num', function() {
+    const feedback_num = get(this, 'result.feedback_num');
+    return feedback_num === 0 || feedback_num > 1;
+  })
 });
