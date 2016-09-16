36914c2d3a691778ff128eb39e49b2cad5f0d402
John Madson <johnmadson@gmail.com>
2016-09-16 13:31:37 -0400

    Added publishedAt to market-detail component (#1327)
    * Added publishedAt to market-detail component
    
    * Removed seconds from formatted-date helper

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 export function formattedDate(params) {
   const date = moment(params[0]);
 
-  return date.format('MMM D, YYYY, h:mm:ss a');
+  return date.format('MMM D, YYYY, h:mm a');
 }
 
 export default Ember.Helper.helper(formattedDate);
