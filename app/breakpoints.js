b1ea037a3e1bc462591b825f120ad57648e95fba
Nik Petersen <demersus@gmail.com>
2016-09-19 10:02:59 -0700

    Place sidebar after content editor when mobile with publish buttons last (#1291)
    * Place sidebar after content editor when mobile with publish buttons
    last
    
    * Use conditional logic to place field groups for mobile vs desktop

--- app/breakpoints.js
+++ app/breakpoints.js
@@ -2,6 +2,8 @@ export default {
   mobile:               '(max-width: 767px)',
   notMobile:            '(min-width: 768px)',
   tablet:               '(min-width: 768px) and (max-width: 991px)',
+  mobileOrTablet:       '(max-width: 992px)',
+  notMobileOrTablet:    '(min-width: 991px)',
   tabletOrSmallDesktop: '(min-width: 768px) and (max-width: 1199px)',
   smallDesktop:         '(min-width: 992px) and (max-width: 1199px)',
   desktop:              '(min-width: 992px)'
