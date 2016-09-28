468739523796eebe01fb01b16bf3292346582773
Nik Petersen <demersus@gmail.com>
2016-09-28 09:23:45 -0700

    Update: confirm_post toast message. (Slide 17)

--- app/pods/lists/confirm-post/route.js
+++ app/pods/lists/confirm-post/route.js
@@ -45,10 +45,10 @@ export default Ember.Route.extend({
 
   sendToIndex() {
     const listservName = get(this, 'listservName');
-    let text =  "It will appear with the rest of the email-only posts in tomorrow's digest but unfortunately not on dailyUV.<br />";
+    let text =  "It will appear with the rest of the email-only posts in tomorrow's digest but not on dailyUV.<br />";
     text +=     "<strong class='u-textBold'>If you would like it to be included on dailyUV and stand out in the digest...</strong><br />";
-    text +=     `Try choosing "Enhance My Post" or <a class="u-textUnderline" href="/sign_up">sign-up</a> to share your post on dailyUV.com and the ${listservName}.<br />`;
-    text +=     `Check out tomorrow's ${listservName} digest to see the difference.`;
+    text +=     `Next time try choosing "ADD PHOTOS/EDIT". See the difference in tomorrow's digest...<br />`;
+    text +=     `Thanks for posting!`;
     const title = `Your Post has been SENT to the ${listservName}.`;
     const options = get(this, 'toastOptions');
 
