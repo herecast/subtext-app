a0faf7328012384e9aa89fe3b4c1d25d446f30aa
gfmartinez <g@briel.us>
2016-10-07 12:16:30 -0400

    css fix for mobile avatar image caret (#1367)
    switch caret to inside and PR consideration
    
    trial for tests
    
    fix

--- app/pods/components/avatar-image/component.js
+++ app/pods/components/avatar-image/component.js
@@ -6,11 +6,12 @@ const { get, set, computed, String: { htmlSafe }  } = Ember;
 
 export default Ember.Component.extend({
   classNames: 'AvatarImage',
-  classNameBindings: ['customSized:custom-sized'],
+  classNameBindings: ['customSized:custom-sized', 'hasCaret:has-caret'],
 
   imageUrl: null,
   userName: null,
   customSize: null,
+  hasCaret: false,
 
   userInitials: computed('userName', function() {
     return computedInitials(get(this, 'userName'));
@@ -29,6 +30,18 @@ export default Ember.Component.extend({
       style = `width:${customSize}px; height:${customSize}px; line-height:${customSize}px; font-size:${fontSize}px`;
       set(this, 'customSized', true);
     }
+    return htmlSafe(style);
+  }),
+
+  customCaretStyle: computed('hasCaret', function() {
+    const customSize = get(this, 'customSize');
+    let style = '';
+
+    if (get(this, 'hasCaret')) {
+      let fontSize = Math.round(0.4 * customSize);
+      style = `width:${fontSize}px; right:-${fontSize}px; line-height:${customSize}px; font-size:${fontSize}px`;
+    }
+    
     return htmlSafe(style);
   })
 });
