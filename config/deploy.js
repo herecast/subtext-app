4999908478c36a0af7a04e3e791f466eab0a79af
Nik Petersen <demersus@gmail.com>
2016-12-27 09:45:26 -0800

    Revert back to Front End Builds dependent deploy configuration. (#1447)
    https://app.clubhouse.io/subtext/story/2796/create-fastboot-feb-branch-to-allow-further-feb-deploys

--- config/deploy.js
+++ config/deploy.js
@@ -1,3 +1,11 @@
+var app = 'ux2';
+var qaHost = 'https://qa-consumer.subtext.org';
+var stagingHost = 'https://stage-consumer.subtext.org';
+var productionHost = 'https://dailyuv.com';
+var path = require('path');
+var sshkey = process.env['EMBER_SSH_KEY'] ? process.env['EMBER_SSH_KEY'] : path.join(process.env['HOME'], '.ssh/id_rsa');
+
+
 module.exports = function(deployTarget) {
   var ENV = {
     build: {},
@@ -8,26 +16,35 @@ module.exports = function(deployTarget) {
       region: 'us-east-1',
       prefix: 'dist',
     },
+    'front-end-builds': {
+      app: app,
+      privateKey: sshkey
+    }
   };
 
   if (deployTarget === 'development') {
     ENV.build.environment = 'development';
+    ENV['front-end-builds'].endpoint = 'http://localhost:3000';
   }
 
   if (deployTarget === 'qa') {
     ENV.build.environment = 'production';
+    ENV['front-end-builds'].endpoint = qaHost;
   }
 
   if (deployTarget === 'staging') {
     ENV.build.environment = 'production';
+    ENV['front-end-builds'].endpoint = stagingHost;
   }
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
+    ENV['front-end-builds'].endpoint = productionHost;
   }
 
   if (deployTarget == 'fe') {
     ENV.build.environment = 'production';
+    ENV['front-end-builds'].endpoint = process.env['FE_ENDPOINT'];
   }
 
   return ENV;
