f4cbc5d4a96a06d0ab65583425256cd55dee7d71
sheehan1102 <ben.sheehan@subtext.org>
2017-01-09 14:26:50 -0500

    Feature/add gtm tags for market digest cta (#1464)
    * GTM tracking for Market Digest CTA [ch3716]
    
    * Adjusting method locale
    
    * Inlining GTM method
    
    * Fix failing tests

--- app/pods/components/subscribe-banner/component.js
+++ app/pods/components/subscribe-banner/component.js
@@ -1,5 +1,23 @@
 import Ember from 'ember';
+import InViewportMixin from 'ember-in-viewport';
+/* global dataLayer */
 
-export default Ember.Component.extend({
-  
+const {
+	set,
+	get
+} = Ember;
+
+export default Ember.Component.extend(InViewportMixin, {
+	canSendImpression: true,
+
+  didEnterViewport() {
+  	if (get(this, 'canSendImpression')) {
+  		if (typeof dataLayer !== "undefined") {
+	  		dataLayer.push({
+	  			'event': 'market-digest-cta-impression'
+	  		});
+	  	}
+  		set(this, 'canSendImpression', false);
+  	}
+  },
 });
