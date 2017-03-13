552f90362b463ee1889c2de324b3abb39ca07b5f
Nik Petersen <demersus@gmail.com>
2017-03-13 06:45:01 -0700

    Updated market workflow with stubbed validations

--- app/pods/components/listserv-market-form/component.js
+++ app/pods/components/listserv-market-form/component.js
@@ -1,9 +1,30 @@
 import Ember from 'ember';
+import TestSelector from 'subtext-ui/mixins/components/test-selector';
 
-export default Ember.Component.extend({
+const {
+  get,
+  Component
+} = Ember;
+
+export default Component.extend(TestSelector, {
+  tagName: 'form',
+
+  submit() {
+    // browser event
+    const save = get(this, 'save');
+
+    if(save) {
+      const cs = get(this, 'changeset');
+      save(cs);
+    }
+
+    return false;
+  },
+
   actions: {
-    afterDetails() {
-      this.attrs.doneEditing();
+    updateContent(content) {
+      get(this, 'changeset').set('content', content);
     }
   }
+
 });
---
+++ app/validations/market.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import ContentValidations from 'subtext-ui/validations/content';
+
+const {assign} = Ember;
+
+export default assign({}, ContentValidations, {
+
+});
