5d215db8325fd7e317ae3c202f39be505cad008c
Nik Petersen <demersus@gmail.com>
2017-03-20 21:51:12 -0700

    get rid of some code redundancy, and add costType rollback

--- app/pods/components/event-details-inline-edit/component.js
+++ app/pods/components/event-details-inline-edit/component.js
@@ -4,20 +4,22 @@ import TestSelector from 'subtext-ui/mixins/components/test-selector';
 export default Ember.Component.extend(TestSelector, {
   actions: {
     rollbackVenue(changeset) {
-      delete changeset.get('changes')['venueId'];
-      delete changeset.get('changes')['venueName'];
-      delete changeset.get('changes')['venueAddress'];
-      delete changeset.get('changes')['venueCity'];
-      delete changeset.get('changes')['venueState'];
-      delete changeset.get('changes')['venueZip'];
-      delete changeset.get('changes')['venueUrl'];
+      const changes = changeset.get('changes');
+      delete changes['venueId'];
+      delete changes['venueName'];
+      delete changes['venueAddress'];
+      delete changes['venueCity'];
+      delete changes['venueState'];
+      delete changes['venueZip'];
+      delete changes['venueUrl'];
 
       changeset.validate('venueId');
     },
 
     rollbackEventCost(changeset) {
-      delete changeset.get('changes')['cost'];
-      delete changeset.get('changes')['cost'];
+      const changes = changeset.get('changes');
+      delete changes['cost'];
+      delete changes['costType'];
     }
   }
 });
