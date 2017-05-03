13ee4b23d6ef80db51c8ef25f0976907d59ba1fa
Christopher Milne <christopher.milne@gmail.com>
2017-05-03 10:49:16 -0400

    updates testem config to use dot reporter (#1649)

--- testem.js
+++ testem.js
@@ -3,6 +3,7 @@ module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
+  "reporter": "dot",
   "launch_in_ci": [
     "PhantomJS"
   ],
