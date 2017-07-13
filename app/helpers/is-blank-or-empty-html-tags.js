9936b5ca27cb7a35bc0875186d059eff477e1cca
Nik Petersen <demersus@gmail.com>
2017-07-13 06:19:17 -0700

    Use cheerio, not $ in helper (#1722)

--- app/helpers/is-blank-or-empty-html-tags.js
+++ app/helpers/is-blank-or-empty-html-tags.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-
+import cheerio from 'npm:cheerio';
 
 export function isBlankOrEmptyHtmlTags(params) {
   var blank = Ember.isEmpty( params[0] );
@@ -7,7 +7,7 @@ export function isBlankOrEmptyHtmlTags(params) {
   if( !blank ) {
     //params is the string, typically wrapped in <p></p> tags so never empty
     //check to see if wrapped
-    let children = Ember.$("<div>" + params[0] + "</div>").children();
+    let children = cheerio("<div>" + params[0] + "</div>").children();
     //if no wraps, is the content there?
     if( children.length <= 0 && params[0].length > 0 ) {
        return false;
@@ -17,8 +17,8 @@ export function isBlankOrEmptyHtmlTags(params) {
     //if children exist, we need to make sure at least one is non empty
     var inside;
 
-    Ember.$.each(children, function(index, el){
-      inside = Ember.$(el).text();
+    children.each(function(i, el){
+      inside = cheerio(el).text();
       if(inside.length > 0) {
         blank = false;
         return;//out of each loop only
