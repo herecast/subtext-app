d43bfb9433ee191fc2351b3971e4b1dd8e3b9711
Jordan Metzmeier <titan8990@gmail.com>
2017-07-14 13:04:45 -0500

    add API_BASE_URL to org content request (#1729)

--- app/adapters/organization-content.js
+++ app/adapters/organization-content.js
@@ -3,6 +3,6 @@ import config from 'subtext-ui/config/environment';
 
 export default ApplicationAdapter.extend({
   urlForQuery(query) {
-    return `/${config.API_NAMESPACE}/organizations/${query.organization_id}/contents`;
+    return `${config.API_BASE_URL}/${config.API_NAMESPACE}/organizations/${query.organization_id}/contents`;
   }
 });
