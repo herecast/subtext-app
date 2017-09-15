48a400682ee6df7594e1dbc5e6ca3c44f7164d0f
Nik Petersen <demersus@gmail.com>
2017-09-15 06:58:58 -0700

    Fix missing location selector, which broke all ugc (#1788)

---
+++ app/pods/components/location-radius-control/component.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+const {get, set} = Ember;
+
+export default Ember.Component.extend({
+  radius: 10,
+
+  onChooseRadius: null,
+  onChooseLocation: null,
+  onNearbyLocationsChanged: null,
+
+  actions: {
+    chooseRadius(radius) {
+      set(this, 'radius', radius);
+      const onChooseRadius = get(this, 'onChooseRadius');
+      if (onChooseRadius) {
+        onChooseRadius(radius);
+      }
+    },
+    chooseLocation(location) {
+      set(this, 'location', location);
+      const onChooseLocation = get(this, 'onChooseLocation');
+      if (onChooseLocation) {
+        onChooseLocation(location);
+      }
+    },
+  }
+});
