d198828511ed9be9fa8b188e7616b7be92ba22a1
Nik Petersen <demersus@gmail.com>
2017-11-30 10:40:13 -0800

    Do not load similar content in fastboot (#1859)

--- app/components/similar-content.js
+++ app/components/similar-content.js
@@ -1,42 +1,44 @@
 import Ember from 'ember';
 import ExpandableContent from '../mixins/components/expandable-content';
-import FastbootExtensions from 'subtext-ui/mixins/fastboot-extensions';
 import {chunk} from 'lodash';
 
-const { get, computed, inject, on, isPresent } = Ember;
+const { get, set, computed, inject, on, isPresent } = Ember;
 
-export default Ember.Component.extend(ExpandableContent, FastbootExtensions, {
+export default Ember.Component.extend(ExpandableContent, {
   classNames:[ 'SimilarContent'],
 
   contentModel: Ember.inject.service('content-model'),
   api: inject.service('api'),
+  fastboot: inject.service('fastboot'),
+  content: null,
 
   getSimilarContent: on('init', function() {
-    const contentId = this.get('contentId');
-    const api = get(this, 'api');
+    set(this, 'content', []);
 
-    if (contentId) {
-      const promise = api.getSimilarContent(contentId).then(({similar_content}) => {
-        if(isPresent(similar_content)) {
-          const contentModel = this.get('contentModel');
+    if(!get(this, 'fastboot.isFastBoot')) {
+      const contentId = this.get('contentId');
+      const api = get(this, 'api');
+
+      if (contentId) {
+        api.getSimilarContent(contentId).then(({similar_content}) => {
+          if(isPresent(similar_content)) {
+            const contentModel = this.get('contentModel');
 
 
-          // Filter out any events that do not have a start date. These are events
-          // that have been imported and do not have event instances associated
-          // with them.
-          const contents = similar_content.filter((record) => {
-            return !(record.content_type === 'event' && Ember.isBlank(record.starts_at));
-          }).map((record) => {
-            return contentModel.convert(record);
-          });
+            // Filter out any events that do not have a start date. These are events
+            // that have been imported and do not have event instances associated
+            // with them.
+            const contents = similar_content.filter((record) => {
+              return !(record.content_type === 'event' && Ember.isBlank(record.starts_at));
+            }).map((record) => {
+              return contentModel.convert(record);
+            });
 
-          this.set('content', contents);
-          this.set('sourceContentId', contentId);
-        }
-      });
-
-      // ensure fastboot waits for promise before rendering
-      this.deferRenderingIfFastboot(promise);
+            this.set('content', contents);
+            this.set('sourceContentId', contentId);
+          }
+        });
+      }
     }
   }),
 
