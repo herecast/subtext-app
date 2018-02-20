1c5088ad9cb215da0cdc9debc81dbe43d9310e36
sheehan1102 <ben.sheehan@subtext.org>
2018-02-20 11:18:48 -0500

    Re-add SimilarContenClick tracking (#1938)
    * Re-add SimilarContenClick tracking
    
    * Change action name

--- app/pods/components/small-card/component.js
+++ app/pods/components/small-card/component.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
+const { get, inject } = Ember;
+
 export default Ember.Component.extend({
   classNames: ['SmallCard'],
+  tracking: inject.service(),
+
+  isSimilarContent: false,
+
+  actions: {
+    trackClick(content) {
+      if (get(this, 'isSimilarContent')) {
+        get(this, 'tracking').push({
+          'event'         : "VirtualSimilarContentClick",
+          'content_id'    : get(content, 'id'),
+          'content_title' : get(content, 'title')
+        });
+      }
+    }
+  }
 });
